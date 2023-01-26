1365d5ba6d7bec7cbf604ead31b19bdbe65fb777
alice <lissa.faul@mail.ru>
2023-01-26 16:17:32 +0300

    TP-4 create admin-api gateway

---
+++ src/main/java/transportation/TransportationAdminApiGatewayApplication.java
@@ -0,0 +1,13 @@
+package transportation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.reactive.config.EnableWebFlux;
+
+@SpringBootApplication
+public class TransportationAdminApiGatewayApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(TransportationAdminApiGatewayApplication.class, args);
+  }
+}
